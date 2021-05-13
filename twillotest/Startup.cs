c5166317e961d70011e32a204c64a9c31bfbefad
Deepak <career.deepaksharma@gmail.com>
2021-05-13 18:30:01 +0530

    this is my fisrt commit

---
+++ twillotest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(twillotest.Startup))]
+namespace twillotest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
